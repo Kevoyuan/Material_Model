fb1e17708f6b29b631f22d97ab3110bc1163c035
Kevoyuan <kevinsyuan@gmail.com>
2023-01-27 17:41:11 +0100

    modified input data

--- make_dataset/program_files_for_CNN/keep_centerline.py
+++ make_dataset/program_files_for_CNN/keep_centerline.py
@@ -3,18 +3,18 @@ import pandas as pd
 import os
 
 # Specify the main directory where the CSV files are located
-main_dir = 'roughmodel_1024/dataset_inp'
+main_dir = 'roughmodel_test/dataset_inp'
 
 # Get total number of files
-total_files = sum(len(files) for _, _, files in os.walk(main_dir))/3
+total_files = 4096
 
 # Initialize tqdm progress bar
 with tqdm(total=total_files, unit='file') as pbar:
     # Iterate through all the files in the main directory and its subfolders
     for dirpath, dirnames, filenames in os.walk(main_dir):
         for filename in filenames:
-            # Check if the file is a CSV and its name starts with "dataset_inp_57_"
-            if filename.endswith('.csv') and filename.startswith('dataset_inp_58_'):
+            # Check if the file is a CSV and its name starts with "dataset_inp_57/58_"
+            if filename.endswith('.csv') and filename.startswith('dataset_inp_57_') or filename.endswith('.csv') and filename.startswith('dataset_inp_58_'):
                 # Read the CSV file into a DataFrame
                 df = pd.read_csv(os.path.join(dirpath, filename))
                 
