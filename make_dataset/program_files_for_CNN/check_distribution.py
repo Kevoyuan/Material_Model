850bfa4665693992b30d5a0874936e4c7c4a2f35
Kevoyuan <kevinsyuan@gmail.com>
2023-02-03 14:23:08 +0100

    check distribution

--- make_dataset/program_files_for_CNN/check_distribution.py
+++ make_dataset/program_files_for_CNN/check_distribution.py
@@ -2,17 +2,70 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 # Read the CSV file
-# df = pd.read_csv("Z:/MA\Material_Model/make_dataset/roughmodel_1024/parameter_success.csv")
-df = pd.read_csv("D:\ge24wej\Documents\makedataset/roughmodel2_1024\parameter_success.csv")
+# df = pd.read_csv("Z:/MA\Material_Model/make_dataset/merged_dataset/parameter_success.csv")
+# df = pd.read_csv("merged_dataset\merged_dataset.csv")
+
+df = pd.read_csv("roughmodel_test\merged_dataset.csv")
+
+# df = pd.read_csv("./roughmodel_test/parameter_success.csv")
 
 
 
 # Select the specific columns you want to check
+
 columns_to_check = [
-                    "alpha1","alpha2","alpha3","alpha4",
-                    "alpha5","alpha6","alpha7","alpha8"
+                    # "alpha1",
+                    # "alpha2",
+                    # "alpha3",
+                    # "alpha4",
+                    # "alpha5",
+                    # "alpha6",
+                    # "alpha7", 
+                    # "alpha8",
+
+                    # 'sig_45',
+                    # 'sig_90',
+                    # 'sig_b',
+                    # 'r_00',
+                    # 'r_45',
+                    # 'r_90',
+                    # 'r_b',
+                    # "M"
+                    # "x7__0:sig_45",
+                    # "x7__1:sig_90",
+                    # "x7__2:r_00",
+                    # "x7__3:r_45",
+                    # "x7__4:r_90",
+                    "y__0:M",
+                    "y__1:alpha1",
+                    "y__2:alpha2",
+                    "y__3:alpha3",
+                    "y__4:alpha4",
+                    "y__5:alpha5",
+                    "y__6:alpha6",
+                    "y__7:alpha7",
+                    "y__8:alpha8"
+
+
+                    
                     ]
-data = df[columns_to_check].round(1)
+
+# columns_to_check = [
+                    # 'sig_45',
+                    # 'sig_90',
+                    # 'sig_b',
+                    # 'r_00',
+                    # 'r_45',
+                    # 'r_90',
+                    # 'r_b',
+                    # "M"
+                    # ]
+
+
+
+
+
+data = df[columns_to_check].round(2)
 
 # print(data)
 
@@ -22,9 +75,9 @@ value_distribution = data.value_counts()
 # Plot the value distribution as a histogram
 # value_distribution.plot(kind='bar')
 # print(value_distribution)
-plt.hist(data)
+plt.hist(data,width=0.01)
 plt.legend(data)
-plt.xticks(np.linspace(0.4, 1.7, 14))
+# plt.xticks(np.linspace(0.4, 1.7, 14))
 
 plt.xlabel('value of alpha')
 plt.ylabel('Frequency')
